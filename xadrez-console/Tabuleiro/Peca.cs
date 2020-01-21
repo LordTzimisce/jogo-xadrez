2c11a7f69c3fc5d51c5dd78d28ddc20aed27dfe6
LordTzimisce <willams.trb@gmail.com>
2020-01-21 17:04:34 -0300

    Parte faltante do tabuleiro

--- xadrez-console/Tabuleiro/Peca.cs
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -2,9 +2,21 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace xadrez_console.Tabuleiro
+namespace Tabuleiro
 {
     class Peca
     {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QtoMovimento { get; set; }
+        public Tabuleiro Tab { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            QtoMovimento = 0;
+            Tab = tab;
+        }
     }
 }
