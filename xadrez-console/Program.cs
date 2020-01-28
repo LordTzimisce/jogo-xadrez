732ca6edbf229c33c6bf5f87e98500cef28ab0bf
LordTzimisce <willams.trb@gmail.com>
2020-01-28 16:04:52 -0300

    Criando a classe PosicaoXadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,21 +8,25 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+            /* try
+             {
+                 Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-                tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-                tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
-                tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
-                tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 9));
+                 tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
+                 tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
+                 tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
+                 tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 9));
 
-                Tela.ImprimirTabuleiro(tabuleiro);
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine("Erro ao inserir peça: " + e.Message);
-            }
+                 Tela.ImprimirTabuleiro(tabuleiro);
+             }
+             catch (TabuleiroException e)
+             {
+                 Console.WriteLine("Erro ao inserir peça: " + e.Message);
+             } */
+
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.ToPosicao());
 ;        }
     }
 }
