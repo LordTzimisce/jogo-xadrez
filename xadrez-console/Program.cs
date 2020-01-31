7e727b1f0bc92eef7f667c877e0cfacd5cd1ae58
LordTzimisce <willams.trb@gmail.com>
2020-01-31 17:24:36 -0300

    Melhorando a implementacao do metodo imprimir no tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,25 +8,28 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            /* try
+             try
              {
                  Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
                  tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
                  tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
                  tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
-                 tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 9));
+                tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Branca), new Posicao(3, 5));
+              
 
-                 Tela.ImprimirTabuleiro(tabuleiro);
+
+                Tela.ImprimirTabuleiro(tabuleiro);
              }
              catch (TabuleiroException e)
              {
                  Console.WriteLine("Erro ao inserir peça: " + e.Message);
-             } */
+             } 
 
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            /*PosicaoXadrez pos = new PosicaoXadrez('c', 7);
             Console.WriteLine(pos);
-            Console.WriteLine(pos.ToPosicao());
-;        }
+            Console.WriteLine(pos.ToPosicao()); */
+            ;
+        }
     }
 }
